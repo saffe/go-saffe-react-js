4b7ff3ffaaf27f63fc38dbd1ea405c09e7d13211
Pedro Cruz <pedrovictor.lage@gmail.com>
2025-05-23 10:54:24 -0300

    Revert "feat: add extra data params"
    This reverts commit 1d7681597c9ef11673a2c8f060393cdf7d0e1665.

--- src/components/GoSaffeCapture/index.tsx
+++ src/components/GoSaffeCapture/index.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
-import { 
-  Settings, 
-  ResultNotification,
-  ExtraData,
-  InitFunc,
-  Props
-} from './interfaces'
 
+interface InitFunc {
+  init(apiKey: string, user: string, endToEndId: string, type: string): any
+}
+
+interface Props {
+  captureKey: string
+  user: string
+  endToEndId: string
+  type: 'verification' | 'onboarding'
+}
 declare global {
   interface Window {
     GoSaffe: InitFunc
@@ -16,24 +19,19 @@ declare global {
 export const CaptureComponent = (props: Props) => {
   useEffect(() => {
     if (window && document) {
-
       const existScript = document.getElementById('GoSaffeCaptureComponent')
-
       if (!existScript) {
         const script = document.createElement('script')
         script.id = 'GoSaffeCaptureComponent'
-        script.src = 'https://go.saffe.ai/cdn/latest'
-
         const body = document.getElementsByTagName('body')[0]
+        script.src = 'https://go.saffe.ai/cdn/latest'
         body.appendChild(script)
-
         script.addEventListener('load', () => {
           window.GoSaffe.init(
             props.captureKey,
             props.user,
             props.endToEndId,
-            props.type,
-	    props.extraData || {},
+            props.type
           )
         })
       }
